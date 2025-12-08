6e4f0dea2d9460e8bdc3d3fdde442a9c1f5c46af
Sam <3237003356@qq.com>
2025-12-08 11:54:50 +0800

    公共模块

---
+++ hksc-common/src/main/java/com/hksc/common/result/Result.java
@@ -0,0 +1,39 @@
+package com.hksc.common.result;
+
+import lombok.Data;
+import java.io.Serializable;
+
+@Data
+public class Result<T> implements Serializable {
+    private Integer code;
+    private String message;
+    private T data;
+
+    private Result() {}
+
+    public static <T> Result<T> success(){
+        return success(null);
+    }
+
+    public static <T> Result<T> success(T data){
+        Result<T> result = new Result<T>();
+        result.setCode(200);
+        result.setMessage("操作成功");
+        result.setData(data);
+        return result;
+    }
+
+    public static <T> Result<T> error(String msg){
+        Result<T> result = new Result<T>();
+        result.setCode(500);
+        result.setMessage(msg);
+        return result;
+    }
+
+    public static <T> Result<T> error(Integer code, String msg){
+        Result<T> result = new Result<T>();
+        result.setCode(code);
+        result.setMessage(msg);
+        return result;
+    }
+}
